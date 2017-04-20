2b71978bd5595957b2835c35572fbc1b46207838
Cameron Green <cgree78@gmail.com>
2017-04-20 14:14:01 -0500

    added a few different tests. Still haven't downloaded anything

---
+++ downloadAudioTest.js
@@ -0,0 +1,40 @@
+var Nightmare = require('nightmare');
+const nightmare = Nightmare({show: true});
+var fs = require('fs');
+var http = require('http');
+
+nightmare
+  .viewport(1000, 1000)
+  .useragent("Mozilla/5.0 (Windows NT 6.3; WOW64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome" +
+      "/38.0.2125.111 Safari/537.36")
+  .goto('http://aerotrak.bandcamp.com/album/at-ease')
+  .wait()
+  .click('h4.ft button.download-link')
+  .type('#userPrice', '0')
+  .wait(500)
+  .click('#downloadButtons_download button')
+  .wait()
+  .wait(1000)
+  // .evaluate(function () {
+  //   return {
+  //     name: $('.downloadItemTitle')
+  //       .text()
+  //       .trim(),
+  //     href: $('.downloadGo')
+  //       .prop('href')
+  //       .trim()
+  //   };
+  // }, function (value) {
+  //   var filename = './' + value.name + '.zip';
+  //   var file = fs.createWriteStream(filename);
+  //   var request = http.get(value.href, function (response) {
+  //     response.pipe(file);
+  //   });
+  // })
+  .click('.item-button')
+  .end()
+  .then(function (err, nightmare) {
+    if (err) 
+      return console.log(err);
+    console.log('Done!');
+  });
---
+++ downloadEprintTest.js
@@ -0,0 +1,40 @@
+var useOldDownloadWay = false;
+
+var Nightmare = require('nightmare');
+const nightmare = Nightmare({show: true});
+
+nightmare
+	.goto('http://eprint.iacr.org/2004/152')
+	.evaluate(function ev(old){
+		var el = document.querySelector("[href*='.pdf']");
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", el.href, false);
+		if (old) {
+			xhr.overrideMimeType("text/plain; charset=x-user-defined");
+		} else {
+			xhr.responseType = "arraybuffer";
+		}
+		xhr.send();
+		if (old) {
+			return xhr.responseText;
+		} else {
+			var bytes = [];
+			var array = new Uint8Array(xhr.response);
+			for (var i = 0; i < array.length; i++) {
+				bytes[i] = array[i];
+			}
+			return bytes;
+		}
+	}, function cb(data){
+		var fs = require("fs");
+		if (useOldDownloadWay) {
+			fs.writeFileSync("book.epub", data, "binary");
+		} else {
+			fs.writeFileSync("book.epub", new Buffer(data), "binary");
+		}
+	}, useOldDownloadWay)
+	.run(function (err, nightmare) {
+		if (err) return console.log(err);
+		console.log('Done!');
+	})
+    .end()
