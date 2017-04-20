ac06818604748948cec85e01a0ea7a058114bb37
Cameron Green <cgree78@gmail.com>
2017-04-19 23:24:54 -0500

    Main push. Adding for collab.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # NightmareInstagramScraper
+Script using Nightmare.js to scrape items off Instagram.
+
+## Prerequisites
+* `node.js >= 6`
+
+## Building
+`npm i`
+
+## Running
+`npm start`
+
+## Testing
+`npm test`
+<!--
+## Configuring
+All configuration lies in `config.js`. These options can all be configured using environment variables. This app
+supports [dotenv](https://github.com/motdotla/dotenv).-->
---
+++ instaTest.js
@@ -0,0 +1,25 @@
+const Nightmare = require('nightmare');		
+const nightmare = Nightmare({ show: true });
+const USERNAME = 'cgreen@chadjoneslaw.com'
+const PASSWORD = '1Cameron'
+
+nightmare
+  .goto('https://instagram.com')
+  .click('._fcn8k')
+  .type('[name=username]', USERNAME )
+  .type('[name=password]', PASSWORD)
+  .click('._ah57t')
+  .wait('._8mlbc')
+  .click('._8mlbc')
+  .screenshot('._ovg3g')
+  // .wait('#zero_click_wrapper .c-info__title a')
+  // .evaluate(function () {
+  //   return document.querySelector('#zero_click_wrapper .c-info__title a').href;
+  // })
+  .end()
+  .then(function (result) {
+    console.log(result);
+  })
+  .catch(function (error) {
+    console.error('Search failed:', error);
+  });
