2db6bc217aca3fdecb63cbe0861f41c3c3a0661c
Cameron Green <cgree78@gmail.com>
2017-04-23 16:54:15 -0500

    Added the ability to create a directory for each set of images

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Nightmare = require('nightmare')
 const nightmare = Nightmare({show: false, openDevTools: false});
 const config = require('./config')
+const mkdirp = require('mkdirp');
+
 const URLs = config.URLs
 /** CAREFUL IT'S ASYNCRONYOUS */
 
@@ -14,9 +16,9 @@ let urlNumber = 0
 setup(urlNumber)
 
 /**
-   * Sets up the Nightmare Instance, catches if we are finished
-   * @param {int} urlNumber The number of which url we are navigating to.
-   */
+ * Sets up the Nightmare Instance, catches if we are finished
+ * @param {int} urlNumber The number of which url we are navigating to.
+ */
 
 function setup(urlNumber) {
     if (urlNumber > URLs.length - 1) {
@@ -32,6 +34,7 @@ function setup(urlNumber) {
 
 /**
  * navigates the page to the current URL
+ * also creates the directory for the images to be saved.
  * @param {string} address The url of the current page
  */
 function navigateToAddress(address) {
@@ -40,7 +43,7 @@ function navigateToAddress(address) {
         .wait('._bkw5z')
         .wait('._ovg3g')
         .exists('._8yoiv')
-        /* 
+        /*
             * This piece is supposed to get rid of the popup at the bottom, but doesn't work quite yet.
 
         .then((elementExists) => {
@@ -49,6 +52,11 @@ function navigateToAddress(address) {
          })
          */
         .then(() => {
+            mkdirp(`IMAGES/${URLs[urlNumber].toUpperCase()}`, (err) => {
+                if (err) {
+                    console.error('Could not create directory: ', err)
+                }
+            });
             getLength()
         })
         .catch((error) => {
@@ -91,7 +99,7 @@ function nextImageScreenshot(counter, length) {
         })
         return
     }
-    nightmare.screenshot(`IMAGES/${URLs[urlNumber]}_Screenshot_${counter}.png`)
+    nightmare.screenshot(`IMAGES/${URLs[urlNumber].toUpperCase()}/Screenshot_${counter}.png`)
         .wait('.coreSpriteRightPaginationArrow')
         .click('.coreSpriteRightPaginationArrow')
         .wait('._a012k')
